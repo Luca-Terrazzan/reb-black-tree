2ba888f469d90cdd66f92156d5745acd3566f972
luca-terrazzan <luca.terraz@gmail.com>
2021-03-03 22:02:31 +0100

    added basic insertion

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,4 +1,3 @@
-
 package src;
 
 public class RedBlackTree<T extends Comparable<T>> {
@@ -10,5 +9,33 @@ public class RedBlackTree<T extends Comparable<T>> {
 
     public Node<T> getRoot() {
         return this.root;
+    }
+
+    public RedBlackTree<T> insertNode(Node<T> node) {
+        try {
+            this.findNewNodeLocation(node, this.root);
+        } catch (Exception e) {
+            System.out.println("Error while trying to insert node.");
+        }
+
+        return this;
+    }
+
+    private void findNewNodeLocation(Node<T> newNode, Node<T> startNode) throws Exception {
+        if (newNode.getData().compareTo(startNode.getData()) > 0) {
+            if (startNode.getChild(Position.RIGHT) == null) {
+                startNode.setChildNode(newNode);
+                return;
+            } else {
+                findNewNodeLocation(newNode, startNode.getChild(Position.RIGHT));
+            }
+        } else {
+            if (startNode.getChild(Position.LEFT) == null) {
+                startNode.setChildNode(newNode);
+                return;
+            } else {
+                findNewNodeLocation(newNode, startNode.getChild(Position.LEFT));
+            }
+        }
     }
 }
